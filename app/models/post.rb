a1153b13364b932ad9676ebc370746dd00d3d275
Eleanor Lee <ellie007@ELEANOR-LEEs-MacBook-Pro.local>
2013-09-27 17:55:11 -0500

    added ability to add attachments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 
   has_many :comments
 
-  #has_attached_file :blog_post_pics, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :blog_post_pics, :default_url => "/images/:style/missing.png"
 
 end
