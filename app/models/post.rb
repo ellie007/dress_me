f29c0b68e5a90ccaeb34add3049aacf700d26774
Eleanor Lee <ellie007@ELEANOR-LEEs-MacBook-Pro.local>
2013-10-01 16:34:39 -0500

    fixed missing image in blog

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 
   has_many :comments
 
-  has_attached_file :blog_post_pics, :default_url => "/images/:style/missing.png"
+  has_attached_file :blog_post_pics, :default_url => ""
 
 end
